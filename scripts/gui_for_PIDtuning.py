3ae8ec7b39f72b9170dea25539080f110b346ac2
k24koba_desktop <hozu.kazu.hozu@gmail.com>
2019-08-13 23:18:56 +0900

    add slider to tune gains for position control(hovering)

--- scripts/gui_for_PIDtuning.py
+++ scripts/gui_for_PIDtuning.py
@@ -17,17 +17,23 @@ import math
 from Tkinter import *
 from whipbot_v2.msg import PID_gains
 
-current_PID_gains_posture = [0, 0, 0]
-current_PID_gains_linear_velocity = [0, 0, 0]
-current_PID_gains_angular_velocity = [0, 0, 0]
+current_PID_gains_posture = [0] * 3  # P, I, D
+current_PID_gains_linear_velocity = [0] * 3  # P, I, D
+current_PID_gains_angular_velocity = [0] * 3  # P, I, D
+# P & D for linear position, P for heading
+current_PID_gains_positon_control = [0] * 3
 
-DEFAULT_PID_GAINS_POSTURE = [0, 0, 0]
-DEFAULT_PID_GAINS_LINEAR_VELOCITY = [0, 0, 0]
-DEFAULT_PID_GAINS_ANGULAR_VELOCITY = [0, 0, 0]
+DEFAULT_PID_GAINS_POSTURE = [1800, 0, 5]  # P, I, D
+DEFAULT_PID_GAINS_LINEAR_VELOCITY = [0] * 3  # P, I, D
+DEFAULT_PID_GAINS_ANGULAR_VELOCITY = [0] * 3  # P, I, D
+# P & D for linear position, P for heading
+DEFAULT_PID_GAINS_POSITION_CONTROL = [150, 390, 100]
 
-new_PID_gains_posture = [0, 0, 0]
-new_PID_gains_linear_velocity = [0, 0, 0]
-new_PID_gains_angular_velocity = [0, 0, 0]
+new_PID_gains_posture = [0] * 3  # P, I, D
+new_PID_gains_linear_velocity = [0] * 3  # P, I, D
+new_PID_gains_angular_velocity = [0] * 3  # P, I, D
+# P & D for linear position, P for heading
+new_PID_gains_position_control = [0] * 3
 
 
 class Test(Frame):
@@ -40,6 +46,8 @@ class Test(Frame):
                 new_PID_gains_linear_velocity[i])
             new_PID_gains.pid_gains_for_angular_velocity.append(
                 new_PID_gains_angular_velocity[i])
+            new_PID_gains.pid_gains_for_position_control.append(
+                new_PID_gains_position_control[i])
         pub_new_gains.publish(new_PID_gains)
         del new_PID_gains
 
@@ -123,6 +131,33 @@ class Test(Frame):
         new_PID_gains = PID_gains()
         self.publish_new_gains()
 
+    def set_Pgain_for_linear_position(self, val):
+        global new_PID_gains_position_control
+        # print "slider now at", val
+        val = int(val)
+        new_PID_gains_position_control = list(new_PID_gains_position_control)
+        new_PID_gains_position_control[0] = val
+        new_PID_gains = PID_gains()
+        self.publish_new_gains()
+
+    def set_Dgain_for_linear_position(self, val):
+        global new_PID_gains_position_control
+        # print "slider now at", val
+        val = int(val)
+        new_PID_gains_position_control = list(new_PID_gains_position_control)
+        new_PID_gains_position_control[1] = val
+        new_PID_gains = PID_gains()
+        self.publish_new_gains()
+
+    def set_Pgain_for_heading(self, val):
+        global new_PID_gains_position_control
+        # print "slider now at", val
+        val = int(val)
+        new_PID_gains_position_control = list(new_PID_gains_position_control)
+        new_PID_gains_position_control[2] = val
+        new_PID_gains = PID_gains()
+        self.publish_new_gains()
+
     def reset(self):
         # global DEFAULT_PID_GAINS_POSTURE, DEFAULT_PID_GAINS_LINEAR_VELOCITY
         self.slider_Pgain_pos.set(DEFAULT_PID_GAINS_POSTURE[0])
@@ -134,16 +169,20 @@ class Test(Frame):
         self.slider_Pgain_angVel.set(DEFAULT_PID_GAINS_ANGULAR_VELOCITY[0])
         self.slider_Igain_angVel.set(DEFAULT_PID_GAINS_ANGULAR_VELOCITY[1])
         self.slider_Dgain_angVel.set(DEFAULT_PID_GAINS_ANGULAR_VELOCITY[2])
+        self.slider_Pgain_position.set(DEFAULT_PID_GAINS_POSITION_CONTROL[0])
+        self.slider_Dgain_position.set(DEFAULT_PID_GAINS_POSITION_CONTROL[1])
+        self.slider_Pgain_heading.set(DEFAULT_PID_GAINS_POSITION_CONTROL[2])
 
     def createWidgets(self):
         # scale for posture control gains
         self.slider_Pgain_pos = Scale(self, from_=0, to=5000,
-                                      value=1800,
+                                      # value=1800,
+                                      variable=1800,
                                       orient=HORIZONTAL,
                                       length="8i",
                                       label="P gain for POS",
                                       command=self.set_Pgain_for_posture)
-        self.slider_Pgain_pos.set(current_PID_gains_posture[0])
+        # self.slider_Pgain_pos.set(current_PID_gains_posture[0])
 
         self.slider_Igain_pos = Scale(self, from_=0, to=500,
                                       # value=50,
@@ -151,15 +190,15 @@ class Test(Frame):
                                       length="8i",
                                       label="I gain for POS",
                                       command=self.set_Igain_for_posture)
-        self.slider_Igain_pos.set(current_PID_gains_posture[1])
+        # self.slider_Igain_pos.set(current_PID_gains_posture[1])
 
         self.slider_Dgain_pos = Scale(self, from_=0, to=50,
-                                      value=5,
+                                      # value=5,
                                       orient=HORIZONTAL,
                                       length="8i",
                                       label="D gain for POS",
                                       command=self.set_Dgain_for_posture)
-        self.slider_Dgain_pos.set(current_PID_gains_posture[2])
+        # self.slider_Dgain_pos.set(current_PID_gains_posture[2])
 
         # scale for linear velocity control gains
         self.slider_Pgain_linVel = Scale(self, from_=0, to=100,
@@ -168,7 +207,7 @@ class Test(Frame):
                                          length="8i",
                                          label="P gain for linVel",
                                          command=self.set_Pgain_for_linear_velocity)
-        self.slider_Pgain_linVel.set(current_PID_gains_linear_velocity[0])
+        # self.slider_Pgain_linVel.set(current_PID_gains_linear_velocity[0])
 
         self.slider_Igain_linVel = Scale(self, from_=0, to=100,
                                          # value=50,
@@ -176,7 +215,7 @@ class Test(Frame):
                                          length="8i",
                                          label="I gain for linVel",
                                          command=self.set_Igain_for_linear_velocity)
-        self.slider_Igain_linVel.set(current_PID_gains_linear_velocity[1])
+        # self.slider_Igain_linVel.set(current_PID_gains_linear_velocity[1])
 
         self.slider_Dgain_linVel = Scale(self, from_=0, to=100,
                                          # value=50,
@@ -184,7 +223,7 @@ class Test(Frame):
                                          length="8i",
                                          label="D gain for linVel",
                                          command=self.set_Dgain_for_linear_velocity)
-        self.slider_Dgain_linVel.set(current_PID_gains_linear_velocity[2])
+        # self.slider_Dgain_linVel.set(current_PID_gains_linear_velocity[2])
 
         # scale for angular velocity control gains
         self.slider_Pgain_angVel = Scale(self, from_=0, to=100,
@@ -193,7 +232,7 @@ class Test(Frame):
                                          length="8i",
                                          label="P gain for angVel",
                                          command=self.set_Pgain_for_angular_velocity)
-        self.slider_Pgain_angVel.set(current_PID_gains_angular_velocity[0])
+        # self.slider_Pgain_angVel.set(current_PID_gains_angular_velocity[0])
 
         self.slider_Igain_angVel = Scale(self, from_=0, to=100,
                                          # value=50,
@@ -201,7 +240,7 @@ class Test(Frame):
                                          length="8i",
                                          label="I gain for angVel",
                                          command=self.set_Igain_for_angular_velocity)
-        self.slider_Igain_angVel.set(current_PID_gains_angular_velocity[1])
+        # self.slider_Igain_angVel.set(current_PID_gains_angular_velocity[1])
 
         self.slider_Dgain_angVel = Scale(self, from_=0, to=100,
                                          # value=50,
@@ -209,7 +248,32 @@ class Test(Frame):
                                          length="8i",
                                          label="D gain for angVel",
                                          command=self.set_Dgain_for_angular_velocity)
-        self.slider_Dgain_angVel.set(current_PID_gains_angular_velocity[2])
+        # self.slider_Dgain_angVel.set(current_PID_gains_angular_velocity[2])
+
+        # scale for position control gains
+        self.slider_Pgain_position = Scale(self, from_=0, to=1000,
+                                           # value=50,
+                                           orient=HORIZONTAL,
+                                           length="8i",
+                                           label="P gain for position control",
+                                           command=self.set_Pgain_for_linear_position)
+        # self.slider_Pgain_position.set(current_PID_gains_positon_control[0])
+
+        self.slider_Dgain_position = Scale(self, from_=0, to=1000,
+                                           # value=50,
+                                           orient=HORIZONTAL,
+                                           length="8i",
+                                           label="D gain for position control",
+                                           command=self.set_Dgain_for_linear_position)
+        # self.slider_Dgain_position.set(current_PID_gains_positon_control[1])
+
+        self.slider_Pgain_heading = Scale(self, from_=0, to=1000,
+                                          # value=50,
+                                          orient=HORIZONTAL,
+                                          length="8i",
+                                          label="P gain for heading control",
+                                          command=self.set_Pgain_for_heading)
+        # self.slider_Pgain_heading.set(current_PID_gains_positon_control[2])
 
         self.reset = Button(self, text='reset slider',
                             command=self.reset)
@@ -226,6 +290,9 @@ class Test(Frame):
         self.slider_Pgain_angVel.pack()
         self.slider_Igain_angVel.pack()
         self.slider_Dgain_angVel.pack()
+        self.slider_Pgain_position.pack()
+        self.slider_Dgain_position.pack()
+        self.slider_Pgain_heading.pack()
         self.reset.pack(side=LEFT)
         self.QUIT.pack(side=LEFT, fill=BOTH)
 
@@ -243,14 +310,7 @@ def callback_get_current_gains(current_PID_gains):
     current_PID_gains_posture = current_PID_gains.pid_gains_for_posture
     current_PID_gains_linear_velocity = current_PID_gains.pid_gains_for_linear_velocity
     current_PID_gains_angular_velocity = current_PID_gains.pid_gains_for_angular_velocity
-
-    DEFAULT_PID_GAINS_POSTURE = current_PID_gains_posture
-    DEFAULT_PID_GAINS_LINEAR_VELOCITY = current_PID_gains_linear_velocity
-    DEFAULT_PID_GAINS_ANGULAR_VELOCITY = current_PID_gains_angular_velocity
-
-    new_PID_gains_posture = current_PID_gains_posture
-    new_PID_gains_linear_velocity = current_PID_gains_linear_velocity
-    new_PID_gains_angular_velocity = current_PID_gains_angular_velocity
+    current_PID_gains_positon_control = current_PID_gains.pid_gains_for_position_control
 
     rospy.loginfo("current gains for posture : " +
                   str(current_PID_gains_posture))
@@ -258,6 +318,8 @@ def callback_get_current_gains(current_PID_gains):
                   str(current_PID_gains_linear_velocity))
     rospy.loginfo("current gains for angular velocity : " +
                   str(current_PID_gains_angular_velocity))
+    rospy.loginfo("current gains for position control : " +
+                  str(current_PID_gains_positon_control))
     print("")
 
 
